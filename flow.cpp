497dbaee8ecf16904e7b7f93be96270532876b6c
Krivoss <jakub.kriv@gmail.com>
2022-10-20 11:35:03 +0200

    added packet processing code from ipk_proj

--- flow.cpp
+++ flow.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 class prog_args {
     public:
-        FILE* file;
+        string file_name;
+        pcap_t* file;
         string netflow_collector;
         int port;
         int active_t;
@@ -12,7 +13,7 @@ class prog_args {
         int flow_cache_size;
 
         prog_args() {
-            file = stdin;
+            file_name = "-";
             netflow_collector = "127.0.0.1";
             port = 2055;
             active_t = 60;
@@ -27,12 +28,10 @@ class prog_args {
                 switch(c) 
                 {
                     case 'f':
-                        file = fopen(optarg, "rw");
-                        if(file == NULL) {
-                            cerr << "ERROR: invalid file was input" << endl;
-                        }
+                        file_name = optarg;
                         break;
                     case 'c':
+                        // TODO get host by name
                         arg_s.assign(optarg);
                         if(arg_s.find(":") == string::npos) {
                             netflow_collector = arg_s;
@@ -57,10 +56,185 @@ class prog_args {
                         exit(0);
                         break;
                     default:
-                        cout << "Unknown parse returns: " << c << endl; 
+                        cout << "Unknown parse returns: " << c << endl;
+                        exit(1);
                         break; 
                 } 
             }
+        }
+
+        void open_file() {
+            char errbuf[PCAP_ERRBUF_SIZE];
+            file = pcap_open_offline(file_name.c_str(), errbuf);
+            if(file == NULL) {
+                cerr << "ERROR: " << errbuf << endl;
+            }
+        }
+
+        void cleanup() {
+            pcap_close(file);
+        }
+};
+
+class packet {
+    public:
+        struct pcap_pkthdr header;
+	    const u_char *packet;
+        string type;
+        string time_s;
+        string src_MAC;
+        string dst_MAC;
+        string src_ip;
+        string dst_ip;
+        u_int16_t src_port;
+        u_int16_t dst_port;
+
+        int process_packet(pcap_t *handle) {
+            int end = read_packet(handle);
+            if (end) {
+                return 1;
+            }
+            
+            packet_print();
+            return 0;          
+        }
+
+        // catches packet and gets info for later output
+        int read_packet(pcap_t *handle) {
+            packet = pcap_next(handle, &header);
+            if (packet == NULL) {
+                return 1;
+            }
+
+            struct ether_header* eth_h = (struct ether_header*)(packet);
+            for (int i: eth_h->ether_shost) {
+                stringstream stream;
+                stream << std::setfill ('0') << setw(2) << hex << i;
+                src_MAC += stream.str() + ":";
+            }
+            src_MAC.pop_back();
+            for (int i: eth_h->ether_dhost) {
+                stringstream stream;
+                stream << std::setfill ('0') << setw(2) << hex << i;
+                dst_MAC += stream.str() + ":";
+            }   
+            dst_MAC.pop_back();
+
+            switch (ntohs(eth_h->ether_type)) {
+                case ETHERTYPE_IP: { // IPv4
+                    const struct iphdr* ip_header = (struct iphdr*)(packet + sizeof(struct ethhdr));
+                    u_short ip_header_len = (ip_header->ihl) * 4;
+
+                    struct in_addr addr_src_bin;
+                    addr_src_bin.s_addr = ip_header->saddr;
+                    src_ip = inet_ntoa(addr_src_bin);
+
+                    struct in_addr addr_dest_bin;                    
+                    addr_dest_bin.s_addr = ip_header->daddr;                  
+                    dst_ip = inet_ntoa(addr_dest_bin);
+                    switch (ip_header->protocol) {
+                        case IPPROTO_TCP: { // TCP
+                            const struct tcphdr* tcp_header {(struct tcphdr*)(packet + sizeof(struct ethhdr) + ip_header_len)};
+                            src_port = ntohs(tcp_header->source);
+                            dst_port = ntohs(tcp_header->dest);                            
+                            type = "TCP";
+                            break;
+                        }
+                        case IPPROTO_UDP: { // UDP
+                            const struct udphdr* udp_header {(struct udphdr*)(packet + sizeof(struct ethhdr) + ip_header_len)};
+                            src_port =  ntohs(udp_header->source);
+                            dst_port = ntohs(udp_header->dest);
+                            type = "UDP";
+                            break;
+                        }
+                    }
+                    break;
+                }
+                default: {
+                    cout << "GOT NON LINKTYPE_ETHERNET PACKET" << endl;
+                    exit(1);
+                }
+            }
+            return 0;
+        }
+
+        // converts unprintable chars to dots
+        string get_printable() {
+            string printable = "";
+            for (bpf_u_int32 i = 0; i < header.len; i++){
+                char c = packet[i];
+                if(c < 32 || c >= 126) {
+                    printable += '.';
+                } 
+                else {
+                    printable += c;
+                }
+            }
+            return printable;
+        }
+
+        void get_timestamp() {
+            // modified code, take from https://stackoverflow.com/questions/48771851/im-trying-to-build-an-rfc3339-timestamp-in-c-how-do-i-get-the-timezone-offset
+            struct tm *p = localtime((const time_t*)&header.ts.tv_sec);
+            char time[100];
+            size_t len = strftime(time, sizeof time - 1, "%FT%T%z", p);
+            // move last 2 digits
+            if(len >= 0) {
+                char minute[] = { time[len-2], time[len-1], '\0' };
+                sprintf(time + len - 2, ":%s", minute);
+            }
+            // end of taken code
+            time_s = time;
+        }
+        
+        // outputs packet respresentation
+        void packet_print() {
+            get_timestamp();
+
+            cout <<  "timestamp: " << time_s << endl;
+            cout << "src MAC: " << src_MAC << endl;
+            cout << "dst MAC: " << dst_MAC << endl;
+            cout << "frame length: " << header.len << " bytes" << endl;
+            // TODO
+            // IPv6 is adresses are not supported
+            if (type != "IPv6") {
+                cout << "src IP: " << src_ip << endl;
+                cout << "dst IP: " << dst_ip << endl;
+            }
+            if(type == "TCP" || type == "UDP") {                
+                cout << "src port: " << src_port << endl;
+                cout << "dst port: " << dst_port << endl << endl;
+            }
+
+            auto printable = get_printable();
+            for (bpf_u_int32 i = 0; i < header.len; i++){
+
+                printf("0x%04x ", i);
+                for (int j = 0; j < 16; j++){
+                    if(i + j > header.len) {
+                        cout << "   ";                        
+                    }
+                    else {
+                        printf("%02x ", packet[i+j]);
+                    }
+                }
+                cout << " ";
+
+                for (int j = 0; j < 16; j++){
+                    if (j == 8) {
+                        cout << " ";
+                    }
+                    if(i+j > header.len) {
+                        break;
+                    }                        
+                    else {
+                        cout<<printable[i+j];
+                    }                        
+                }
+                i += 15;
+                cout << endl;
+            }
+            cout << endl;
         }
 };
 
@@ -69,6 +243,17 @@ int main(int argc, char** argv) {
     if(argc > 1) {
         prog_args.process_args(argc, argv);
     }
+    prog_args.open_file();
 
+    int end;
+    while(1) {
+        packet packet_to_process;
+        end = packet_to_process.process_packet(prog_args.file);
+        if (end) {
+            break;
+        }
+    }
+
+    prog_args.cleanup();
     return 0;
 }
--- flow.h
+++ flow.h
@@ -1,4 +1,13 @@
 #include <iostream>
 #include <getopt.h>
+#include <signal.h>
+#include <iomanip>
+#include <sstream>
+
+#include <pcap.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
 
 int main(int argc, char** argv);
